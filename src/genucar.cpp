5fb62b96fbefd8d883af87dcdba91da43dccd4ab
Tomas Kozak <kozakt.kt@gmail.com>
2018-01-23 21:36:17 +0100

    Start working on command parsing

--- src/genucar.cpp
+++ src/genucar.cpp
@@ -9,6 +9,32 @@ const int d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 char line1[17]="                ";
 char line2[17]="                ";
+const int cmdSize=8;
+const int maxParameters=8;
+
+
+
+void executeCommand(const char* cmd, const int parCount, const int* par) {
+
+}
+
+void readCommand(Serial_ s) {
+  if (s.available()>0) {
+    char cmd[cmdSize+1];
+    int numRead, parCount=0;
+    int par[maxParameters];
+    numRead = s.readBytesUntil(' ', cmd, cmdSize);
+    cmd[numRead] = '\0';
+    while (s.available()>0) {
+      par[parCount]=s.parseInt();
+      parCount++;
+    }
+    executeCommand(cmd, parCount, par);
+  }
+}
+
+
+
 
 void setup() {
   // set up the LCD's number of columns and rows:
